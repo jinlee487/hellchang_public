3dfc0dc5af1683c48fad8c4dc6cbd9ccfae49ca2
jinwoo <jlee487@wisc.edu>
2020-07-09 17:34:16 +0900

    17:34 jinwoo local to branch final

---
+++ src/main/java/com/ncs/green/InbodyController.java
@@ -0,0 +1,29 @@
+package com.ncs.green;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import service.MService;
+import vo.MemberVO;
+
+@Controller
+public class InbodyController {
+	@Autowired
+	MService service;
+	
+	
+	
+	
+	
+}
+
+
---
+++ src/main/java/jdbcUtil/InbodyDAO.java
@@ -0,0 +1,32 @@
+package jdbcUtil;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import vo.InbodyVO;
+
+@Repository
+public class InbodyDAO {
+	@Autowired
+	private SqlSession sqlsession;
+	private static final String NS="green.mappers.inbodyMapper.";
+
+	public List<InbodyVO> selectList() {
+		return sqlsession.selectList(NS+"selectList") ;
+	} // selectList()
+	
+	public InbodyVO selectOne(InbodyVO vo) {
+		return sqlsession.selectOne(NS+"selectOne", vo);
+	} // selectList()
+	
+	public int insert(InbodyVO vo) { 
+		return sqlsession.insert(NS+"insert", vo);
+	}
+	
+	public int update(InbodyVO vo) { 
+		return sqlsession.update(NS+"update", vo);
+	}
+}
